64fd8cdedb3eedb7d12957473dfe39d54794a3fb
Numanafsar <nomanafsar46@gmail.com>
2025-01-03 18:19:54 +0500

    env is added

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDX6Pn7SSNcniNZAG4qz7n7CC5HNrsYx_A",
-  authDomain: "parkit-23d8d.firebaseapp.com",
-  databaseURL: "https://parkit-23d8d-default-rtdb.firebaseio.com",
-  projectId: "parkit-23d8d",
-  storageBucket: "parkit-23d8d.firebasestorage.app",
-  messagingSenderId: "69889111926",
-  appId: "1:69889111926:web:b528379805b4923c0c2c57"
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
